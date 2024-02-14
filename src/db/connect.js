2597a83be5e7e6ee8e9b77bb6b15a381d2a77846
Abhishek Kolge <83946567+AbhishekKolge@users.noreply.github.com>
2024-02-14 17:09:27 +0530

    Update connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -5,6 +5,9 @@ const connectDB = (url) => mongoose.connect(url);
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
+   socket: {
+    tls: true,
+  },
 });
 
 module.exports = { connectDB, redisClient };
